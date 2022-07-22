e2f6b1b574f79d03a85d4ca5566d5f96364385c8
del-ui <wambuivictor@students.must.ac.ke>
2022-07-22 08:59:58 +0300

    update

--- app/src/main/java/com/victor/jetpack_ui/MainActivity.kt
+++ app/src/main/java/com/victor/jetpack_ui/MainActivity.kt
@@ -7,16 +7,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.victor.jetpack_ui.ui.theme.Jetpack_UITheme
 
 class MainActivity : ComponentActivity() {
@@ -40,10 +41,23 @@ fun SignInScreen(){
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top) {
             Image(
-                painter = painterResource(id = R.drawable.ic_jet),
+                painter = painterResource(id = R.drawable.ic_visa),
                 contentDescription = "App Logo",
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.weight(1f),
+                colorFilter = ColorFilter.tint(Color.White)
              )
+            Card(
+                Modifier
+                    .weight(2f)
+                    .padding(8.dp),
+            shape = RoundedCornerShape(32.dp)) {
+                Column(
+                    Modifier
+                    .fillMaxSize()
+                    .padding(32.dp)) {
+
+                }
+            }
         }
     }
 }
