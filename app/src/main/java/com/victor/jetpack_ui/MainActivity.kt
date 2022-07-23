736ac98d3026ea99da2fde57fcced148923bd37a
del-ui <wambuivictor@students.must.ac.ke>
2022-07-23 07:15:21 +0300

    update

--- app/src/main/java/com/victor/jetpack_ui/MainActivity.kt
+++ app/src/main/java/com/victor/jetpack_ui/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
